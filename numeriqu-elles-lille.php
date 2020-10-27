40efac345afd884f601f6437fffc968e9be9bb59
Lepez_Rodrigue <lepezrodrigue1@gmail.com>
2020-10-28 00:57:08 +0100

    add partner logo

---
+++ numeriqu-elles-lille.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Numeriqu'<span>Elles </span>Lille</p>
+    <p>Inscription via ce lien</p>
+    <input type="button" value="Cliquez ici">
+    <p>Salon virtuel</p>
+    <input type="button" value="Cliquez ici">
+    <p>Collège rendez-vous à 9h00</p>
+    <p>Lycée rendez-vous à 14h00</p>
+    <p>Les ateliers partenaires</p>
+    <img src="les_photos_drive/logo_upcycle.png" alt="logo">
+    <img src="les_photos_drive/euratechnologies.jpeg" alt="logo">
+    <img src="les_photos_drive/logo_ibm.png" alt="logo">
+    <img src="les_photos_drive/AXA_Logo.svg" alt="logo">
+    <img src="les_photos_drive/AXA_Logo.svg" alt="logo">
+    <img src="les_photos_drive/AXA_Logo.svg" alt="logo">
+    <img src="les_photos_drive/AXA_Logo.svg" alt="logo">
+    <img src="les_photos_drive/AXA_Logo.svg" alt="logo">
+    <img src="les_photos_drive/AXA_Logo.svg" alt="logo">
+    <img src="les_photos_drive/logo_Decathlon.jpeg" alt="logo">
+    <img src="les_photos_drive/logo_Decathlon.jpeg" alt="logo">
+    <img src="les_photos_drive/logo_Decathlon.jpeg" alt="logo">
+    <img src="les_photos_drive/orientoi.png" alt="logo">
+    <img src="les_photos_drive/logo_simplon.png" alt="logo">
+    <img src="les_photos_drive/logo_simplon.png" alt="logo">
+    <img src="les_photos_drive/logo_laredoute.png" alt="logo">
+    <img src="les_photos_drive/logo_laredoute.png" alt="logo">
+    <img src="les_photos_drive/logo_laredoute.png" alt="logo">
+    <img src="les_photos_drive/logo_laredoute.png" alt="logo">
+    <img src="les_photos_drive/logo_facemel.png" alt="logo">
+    <img src="les_photos_drive/" alt="pop-school">
+    <img src="les_photos_drive/corif.png" alt="logo">
+    <img src="les_photos_drive/logo_fondation_norsys.png" alt="logo">
+    <img src="les_photos_drive/logo_epitech.png" alt="logo">
+    <img src="les_photos_drive/logo_fi.png" alt="logo">
+    <img src="les_photos_drive/Logo_Hauts-de-France_2016.svg" alt="logo">
+    <img src="les_photos_drive/rte_nord.jpeg" alt="logo">
+    <img src="les_photos_drive/logo_inria.png" alt="logo">
+    <img src="les_photos_drive/logo_kiloutou.png" alt="logo">
+    <img src="les_photos_drive/logo_orange.png" alt="logo">
+    <p>Questionnaire de satisfaction</p>
+    <p>Pour nous aider a améliorer nos atelier, veuillez répondre au questionnaire de satifaction qui est anonyme, en le téléchargeant ci-dessous et en le renvoyant à Maleka a l'adressse mail suivant:</p>
+    <input type="button" value="Télécharger le questionnaire">
+</body>
+</html>
+
