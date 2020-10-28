dacc3828c0df0987f40e1a8384f36871fea9f68e
Lepez_Rodrigue <lepezrodrigue1@gmail.com>
2020-10-29 00:17:33 +0100

    add text

--- numeriqu-elles-lens.php
+++ numeriqu-elles-lens.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="numeriqu-elles.css">
-    <title>Numeriqu'elles Lille</title>
+    <title>Numeriqu'elles Lens</title>
 </head>
 
 <body>
@@ -27,6 +27,30 @@
     <p class='lock'>Lycée rendez-vous à 14h00</p>
     <h2>Les ateliers partenaires</h2>
     <div class="container">
+    <div>
+            <a href="https://www.orientoi.fr/"><img src="les_photos_drive/orientoi.png" alt="logo"></a>
+            <p> l’atelier <br>
+                Jeux des talents <br>
+                découverte des métiers du <br>
+                numérique présentation <br>
+                du fonctionnement de <br>
+                l'intelligence artificille</p>
+        </div>
+        <div>
+            <a href="https://www.norsys.fr/"><img src="les_photos_drive/logo_fondation_norsys.png" alt="logo"></a>
+            <p> l’atelier <br>
+                Casser les stéréotypes <br>
+                des métiers de l'informatique</p>
+        </div>
+        <div>
+            <a href="https://www.epitech.eu/"><img src="les_photos_drive/logo_epitech.png" alt="logo"></a>
+            <p> l’atelier <br>
+                Pauline témoigne: <br>
+                pourquoi/comment le choix <br> 
+                du numérique/programation <br> 
+                informatique <br>  
+                illustrations concrètes</p>
+        </div>
     </div>
 
 <h2>Questionnaire de satisfaction</h2>
