5283ad6ee229396614baefd990b2aa227923361a
Lepez_Rodrigue <lepezrodrigue1@gmail.com>
2020-10-28 09:16:35 +0100

    add text

---
+++ numeriqu'elles-maubeuge.php
@@ -0,0 +1,21 @@
+<?php
+    require_once('header.html');
+?>
+
+    <p>Numeriqu'<span>Elles </span>Lille</p>
+    <p>Inscription via ce lien</p>
+    <input type="button" value="Cliquez ici">
+    <p>Salon virtuel</p>
+    <input type="button" value="Cliquez ici">
+    <p>Collège rendez-vous à 9h00</p>
+    <p>Lycée rendez-vous à 14h00</p>
+    <p>Les ateliers partenaires</p>
+
+
+    <p>Questionnaire de satisfaction</p>
+    <p>Pour nous aider a améliorer nos atelier, veuillez répondre au questionnaire de satifaction qui est anonyme, en le téléchargeant ci-dessous et en le renvoyant à Maleka a l'adressse mail suivant:</p>
+    <input type="button" value="Télécharger le questionnaire">
+<?php
+    require_once('footer.html');
+?>
+
