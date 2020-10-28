13580cb7228e4d85a4d63d8ef697af34382047d7
Lepez_Rodrigue <lepezrodrigue1@gmail.com>
2020-10-28 23:01:33 +0100

    rename css

--- numeriqu-elles-lille.php
+++ numeriqu-elles-lille.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="numeriqu-elles-lille.css">
+    <link rel="stylesheet" href="numeriqu-elles.css">
     <title>Numeriqu'elles Lille</title>
 </head>
 
