69bf3736956b17971f4e72ac17721ea40d351fb3
Lepez_Rodrigue <lepezrodrigue1@gmail.com>
2020-10-28 22:54:08 +0100

    add contenu html

--- numeriqu'elles-maubeuge.php
+++ numeriqu'elles-maubeuge.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="numeriqu-elles-maubeuge.css">
+    <title>Numeriqu'elles Lille</title>
+</head>
 <?php
     require_once('header.html');
 ?>
