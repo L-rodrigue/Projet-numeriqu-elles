3f108a02e31ce18988739cd1a9cb65af1367975c
Lepez_Rodrigue <lepezrodrigue1@gmail.com>
2020-10-28 23:56:54 +0100

    investment from element

--- numeriqu'elles-maubeuge.php
+++ numeriqu'elles-maubeuge.php
@@ -4,26 +4,86 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="numeriqu-elles-maubeuge.css">
+    <link rel="stylesheet" href="numeriqu-elles.css">
     <title>Numeriqu'elles Lille</title>
 </head>
 <?php
     require_once('header.html');
 ?>
 
-    <p>Numeriqu'<span>Elles </span>Lille</p>
-    <p>Inscription via ce lien</p>
-    <input type="button" value="Cliquez ici">
-    <p>Salon virtuel</p>
-    <input type="button" value="Cliquez ici">
-    <p>Collège rendez-vous à 9h00</p>
-    <p>Lycée rendez-vous à 14h00</p>
-    <p>Les ateliers partenaires</p>
+    <h1>Numeriqu'<span>Elles </span>Maubeuge</h1>
+    <div class='section'>
+        <p class='text'>Inscription via ce lien</p>
+        <input type="button" value="Cliquez ici">
+    </div>
+    <div class='section'>
+        <p class='text'>Salon virtuel</p>
+        <input type="button" value="Cliquez ici">
+    </div>
+    <p class='lock'>Collège rendez-vous à 9h00</p>
+    <p class='lock'>Lycée rendez-vous à 14h00</p>
+    <h2>Les ateliers partenaires</h2>
 
-
-    <p>Questionnaire de satisfaction</p>
-    <p>Pour nous aider a améliorer nos atelier, veuillez répondre au questionnaire de satifaction qui est anonyme, en le téléchargeant ci-dessous et en le renvoyant à Maleka a l'adressse mail suivant:</p>
-    <input type="button" value="Télécharger le questionnaire">
+    <div class="container">
+    <div>
+            <a href="https://www.orientoi.fr/"><img src="les_photos_drive/orientoi.png" alt="logo"></a>
+            <p> l’atelier <br>
+                Initiation au code et à l'art <br> numérique</p>
+        </div>
+        <div>
+            <a href="https://www.gipreussir.fr/"><img src="les_photos_drive/logo_resa.png" alt="logo"></a>
+            <p> l’atelier <br>
+                3 témoignages métiers de <br> femmes dans le numérique</p>
+        </div>
+        <div>
+            <a href="https://pierre-forest-maubeuge.enthdf.fr/"><img src="les_photos_drive/lycée_maubeuge_logo.jpg" alt="logo"></a>
+            <p> l’atelier <br>
+            pour SIN : réalisation d'un <br> quizz en amont et réponses <br> au quizz durant les 30 minutes <br> avec échanges.</p>
+        </div>
+        <div>
+            <a href="https://www.intechinfo.fr/"><img src="les_photos_drive/logo_intech.png" alt="logo"></a>
+            <p> l’atelier <br>
+            2 sujets animés par la même <br> personnes à choisir</p>
+        </div>
+        <div>
+            <a href="https://www.femmes-ingenieurs.org/"><img src="les_photos_drive/logo_fi.jpg" alt="logo"></a>
+            <p> l’atelier <br>
+            présentation d'une vidéo de <br> l'ISEN puis témoignages de <br> femmes ingénieures (activité <br> professionnelle et études)</p>
+        </div>
+        <div>
+            <a href="https://www.axa.fr/"><img src="les_photos_drive/AXA_Logo.svg" alt="logo"></a>
+            <p> l’atelier <br>
+                Quizz sur les différents <br> métiers de l'informatique</p>
+        </div>
+        <div>
+            <a href="https://www.axa.fr/"><img src="les_photos_drive/AXA_Logo.svg" alt="logo"></a>
+            <p> l’atelier <br>
+                Deux équipes doivent <br> répondre à différents types <br> de questions, et battre "un dragon"</p>
+        </div>
+        <div>
+            <a href="https://e-catalyst.fr/"><img src="les_photos_drive/logo_catalyst.png" alt="logo"></a>
+            <p> l’atelier <br>
+                - Une série de  vidéos d'environ 1min à 1min30 <br> chacune de présentation de l'équipe évoquant leur métier. <br>
+                - Un sous atelier collaboratif pour imaginer les métiers <br> du numérique de demain ou <br> une visite virtuelle sur la sensibilisation<br>  autour des bonnes pratiques du numérique <br>
+                - Un quizz pour finaliser l'atelier <br> sur les bonnes pratiques du numérique</p>
+        </div>
+        <div>
+            <a href="https://www.citedesgeometries.org/newcdg/"><img src="les_photos_drive/logo_geometris.png" alt="logo"></a>
+            <p> l’atelier <br>
+                Atelier virtuel de la découpeuse graveuse <br> laser imprimante 3D et brodeuse numérique</p>
+        </div>
+    </div>
+    <h2>Questionnaire de satisfaction</h2>
+    <div class='satifaction'>
+        <div class='text-satifaction'>
+            <p>Pour nous aider a améliorer nos atelier, veuillez répondre au questionnaire de satifaction qui est
+                anonyme, en le
+                téléchargeant ci-dessous et en le renvoyant à Maleka a l'adressse mail suivant:</p>
+        </div>
+        <div>
+            <input type="button" value="Télécharger le questionnaire">
+        </div>
+    </div>
 <?php
     require_once('footer.html');
 ?>
