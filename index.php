a1de9ddcf696014d9834802fe3a181ccbc05441e
Jose59300 <j.freirelopes26@gmail.com>
2020-10-28 11:47:56 +0100

    Page index desktop

---
+++ index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>Numériqu'elles</title>
+</head>
+<body>
+  <?php
+  require_once('header.html');
+  ?>
+  <section class="flexbox">
+    <h2 class="purple">Constats</h2>
+    <h2 class="green">et</h2>
+    <h2 class="purple">Objectifs</h2>
+  </section>
+  <section class="flexbox">
+    <div class="findings">
+      <p>Un manque notable de femmes
+        dans les métiers du numérique
+        et de l'informatique.</p><br>
+        <p>Les femmes représentent 33% de la branche du numérique.
+          Ce chiffre tombe à 18% dans
+          l'informatique.</p>
+        </div>
+        <div class="goals">
+          <p>Dans  les  Hauts-de-France,  3000  emplois  numériques  sont  non  pourvus  chaque  année  et  les  femmes  représentent  seulement  33%  des  emplois  du  secteur  et  18%  des  métiers  informatiques.  Ces  métiers  restent  mal  connus  et  stéréotypés  :  maths,  urbain,  geek... masculins.</p><br>
+          <p>Pour  répondre  à  cet  enjeu  le  Corif  organise  Numériqu’elles  depuis  2015  sous  la  forme  d’ateliers  dédiés  aux  collegiennes  et  lycéennes,  pour  faire  découvrir  les  formations  et  métiers  du  numérique.</p>
+
+        </div>
+      </section>
+      <div class="header_bottom">
+        <img class="rectangle" src="images/Rectangle.svg" alt="Rectangle bleu">
+        <div class="title">
+          <p>édition</p>
+          <p>En distanciel</p>
+          <p>sur 7 territoires</p>
+
+        </div>
+        <div class="trait"></div>
+        <img class="header" src="les_photos_drive/header_edition.jpg" alt="photo jeunes éléves">
+        <section class="flexbox">
+          <h2 class="purple">Au programme</h2>
+          <h2 class="purple"> Édition 2020</h2>
+        </section>
+        <section class="flexbox_edition">
+          <div class="programme">
+            <ul>
+              <li>Temps collectif (ouverture, table ronde)</li>
+              <li>Rotations d’ateliers de 30 minutes sur une demie-journée maximum par petits groupes</li>
+              <li>Animés par des organismes de formations et des entreprises</li>
+              <li>Pour faire découvrir des métiers à travers des expérimentations et des rencontres</li>
+            </ul>
+          </div>
+          <div class="list">
+            <ul>
+              <li><span class="green_list">Lille</span> - 6/11</li>
+              <li><span class="green_list">Soissons</span> - 13/11</li>
+              <li><span class="green_list">Maubeuge</span> - 17/11</li>
+              <li><span class="green_list">Beauvais</span> - 20/11</li>
+              <li><span class="green_list">Boulogne-sur-mer</span> - 24/11</li>
+              <li><span class="green_list">Amiens</span> - 04/12</li>
+              <li><span class="green_list">Lens</span> - 11/12</li>
+            </ul>
+          </div>
+        </section>
+        <section class="flexbox_opportunities">
+          <div class="opportunities">
+            <h2 class="purple">Quelles opportunités ?</h2>
+          </div>
+          <div class="list_opportunities">
+            <li>Rencontrer et dialoguer avec des professionnel.les</li>
+            <li>Découvrir  des  entreprises  du  numérique  ou  des services informatiques de grandes entreprises</li>
+            <li>Expérimenter des outils numériques</li>
+            <li>Se préparer et trouver un stage ; devenir ambassadrice</li>
+
+          </div>
+
+        </section>
+        <div class="header_bottom">
+          <img class="rectangle" src="images/Rectangle.svg" alt="Rectangle bleu">
+          <div class="title">
+            <p>Les ateliers</p>
+
+
+          </div>
+          <div class="trait"></div>
+          <img class="header" src="les_photos_drive/index_atelier.jpg" alt="photo jeunes éléves">
+          <section class="flexbox_workshop">
+            <h2 class="purple">Tester</h2>
+            <h2 class="purple">Découvrir</h2>
+            <h2 class="purple">Expérimenter</h2>
+            <h2 class="purple">Intéragir</h2>
+          </section>
+          <section class="flexbox_workshop">
+            <img class="workshop" src="les_photos_drive/tester.jpg" alt="éléve">
+            <img class="workshop" src="les_photos_drive/decouvrir.jpg" alt="éléve">
+            <img class="workshop" src="les_photos_drive/experimenter.jpg" alt="casque réalité virtuelle">
+            <img class="workshop" src="les_photos_drive/interagir.jpg" alt="groupes de personnes">
+          </section>
+          <section class="event">
+            <h2 class="title_event">Un événement qui rassemble ...</h2>
+            <div class="flexbox_event">
+              <p class="event_green">20</p>
+              <p class="event_green">10</p>
+              <p class="event_green">2073</p>
+              <p class="event_green">984</p>
+            </div>
+            <div class="flexbox_event">
+              <p class="event_text">Entreprises</p>
+              <p class="event_text">Organismes de formation</p>
+              <p class="event_text">Filles déja sensibilisées depuis 2015</p>
+              <p class="event_text">Filles inscrites en 2020</p>
+            </div>
+          </section>
+          <section class="flexbox_organization">
+              <h2 class="purple_organization">Organisation</h2>
+
+            <div class="list_organization">
+              <li>Les rectorats invitent les établissements à faire venir des collégiennes et lycéennes pendant une journée banalisée</li>
+              <li>Les organismes de formations et entreprises proposent des stands attractifs et des animations ludiques</li>
+              <li>Les acteurs locaux se mobilisent sur l’organisation de l’événement</li>
+              <li>Les filles sensibilisées deviennent ambassadrices dans leurs établissements</li>
+            </div>
+          </section>
+
+
+          <div class="header_bottom">
+            <img class="rectangle" src="images/Rectangle.svg" alt="Rectangle bleu">
+            <div class="title">
+              <p>Nos partenaires</p>
+            </div>
+            <div class="trait"></div>
+            <img class="header" src="les_photos_drive/header_edition.jpg" alt="photo jeunes éléves">
+            <img class="partners" src="les_photos_drive/logo_partenaire.png" alt="logo partenaires">
+
+
+
+        <?php
+        require_once('footer.html');
+        ?>
+      </body>
+      </html>
