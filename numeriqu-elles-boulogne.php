31cc23d91a1fbc6bfa2b437fcd649d24d0219ea5
Lepez_Rodrigue <lepezrodrigue1@gmail.com>
2020-10-29 00:34:19 +0100

    add text ans pictures

--- numeriqu-elles-boulogne.php
+++ numeriqu-elles-boulogne.php
@@ -27,8 +27,58 @@
     <p class='lock'>Lycée rendez-vous à 14h00</p>
     <h2>Les ateliers partenaires</h2>
     <div class="container">
+    <div>
+		    <a href="https://www.axa.fr/"><img src="les_photos_drive/AXA_Logo.svg" alt="logo"></a>
+            <p> l’atelier <br>
+                2 sujets animés par <br>
+                la même personne</p>
+        </div>
+        <div>
+				<a href="https://www.axa.fr/"><img src="les_photos_drive/AXA_Logo.svg" alt="logo"></a>
+                <p> l’atelier <br>
+               Cherche des bugs <br>
+                sur une application</p>
+        </div>
+        <div>
+				<a href="https://www.axa.fr/"><img src="les_photos_drive/AXA_Logo.svg" alt="logo"></a>
+                <p> l’atelier <br>
+                Jeu: Découvrir <br>
+                les métiers de l'informatique</p>
+        </div>
+        <div>
+				<a href="https://www.axa.fr/"><img src="les_photos_drive/AXA_Logo.svg" alt="logo"></a>
+                <p> l’atelier <br>
+               Jeu de 'oie</p>
+        </div>
+        <div>
+				<a href="https://www.axa.fr/"><img src="les_photos_drive/AXA_Logo.svg" alt="logo"></a>
+                <p> l’atelier <br>
+                Kahoot</p>
+        </div>
+        <div>
+				<a href="https://www.axa.fr/"><img src="les_photos_drive/AXA_Logo.svg" alt="logo"></a>
+                <p> l’atelier <br>
+               Jeu sur la sécurité <br>
+                informatique </p>
+        </div>
+        <div>
+            <a href="https://www.orientoi.fr/"><img src="les_photos_drive/orientoi.png" alt="logo"></a>
+            <p> l’atelier <br>
+                Jeux des talents <br>
+                découverte des métiers <br>
+                du numérique présentation <br>
+                du fonctionnement de <br>
+                l'intelligence artificielle</p>
+        </div>
+        <div>
+            <a href="https://edouard-branly-boulogne-sur-mer.enthdf.fr/"><img src="les_photos_drive/logo_branly_ok.png" alt="logo"></a>
+            <p> l’atelier <br>
+            Présentation des formations sur le numérique <br>
+            dans les Lycées Générales et Technologiques SNT en seconde, <br>
+            Numérique et Sciences Informatiques en première et Terminale Générale <br>
+            et STID en première et Terminale Technologique</p>
+        </div>
     </div>
-
     <h2>Questionnaire de satisfaction</h2>
     <div class='satifaction'>
         <div class='text-satifaction'>
