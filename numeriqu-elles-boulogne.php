48964ab8c3f584d0ea031a8e39a94a81fb05becd
Lepez_Rodrigue <lepezrodrigue1@gmail.com>
2020-10-29 00:22:25 +0100

    create file

---
+++ numeriqu-elles-boulogne.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="numeriqu-elles.css">
+    <title>Numeriqu'elles Boulogne</title>
+</head>
+
+<body>
+
+
+    <?php
+    require_once('header.html');
+?>
+    <h1>Numeriqu'<span>Elles </span>Lille</h1>
+    <div class='section'>
+        <p class='text'>Inscription via ce lien</p>
+        <input type="button" value="Cliquez ici">
+    </div>
+    <div class='section'>
+        <p class='text'>Salon virtuel</p>
+        <input type="button" value="Cliquez ici">
+    </div>
+    <p class='lock'>Collège rendez-vous à 9h00</p>
+    <p class='lock'>Lycée rendez-vous à 14h00</p>
+    <h2>Les ateliers partenaires</h2>
+    <div class="container">
+    </div>
+
+    <h2>Questionnaire de satisfaction</h2>
+    <div class='satifaction'>
+        <div class='text-satifaction'>
+            <p>Pour nous aider a améliorer nos atelier, veuillez répondre au questionnaire de satifaction qui est
+                anonyme, en le
+                téléchargeant ci-dessous et en le renvoyant à Maleka a l'adressse mail suivant:</p>
+        </div>
+        <div>
+            <input type="button" value="Télécharger le questionnaire">
+        </div>
+    </div>
+    <?php
+    require_once('footer.html');
+?>
+</body>
+
+</html>
