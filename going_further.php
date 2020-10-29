cdb19eede129a8eecc4866485a00253148fccd89
Nelly Anda <anda.nelly@pop.eu.com>
2020-10-29 10:21:25 +0100

    pictures added to pages prepare_yourself and going_further

--- going_further.php
+++ going_further.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
   <link rel="stylesheet" href="going_further.css">
   <title>NUMERIQU'ELLES</title>
 </head>
@@ -11,31 +12,49 @@
   require_once('header.html');
 ?>
   <div>
-    <h1>Ambassadrices <span>du </span>numérique</h1>
+    <h1>Devenez Ambassadrices du <span>Numérique</span></h1>
   </div>
-  <div>
-    <p>Suite au forum Numériqu’elles du 8 novembre 2019, il est proposé aux élèves des établissements ayant participé de réaliser une ou plusieurs productions utilisant le numérique pour promouvoir ce secteur auprès des collégiennes et des lycéennes notamment.<br>
-    L’objectif est de déconstruire les stéréotypes liés au secteur du numérique en présentant par exemple un métier, une association, un parcours personnel et/ou professionnel, une action menée dans l’établissement en lien avec le forum...</p>
-    <p>Les réalisations possibles sont les suivantes :</p>
-    <ul>
-      <li>capsule vidéo de 3 minutes maximum</li>
-      <li>émission d’une webradio</li>
-      <li>page web du site de l’établissement</li>
-      <li>diaporama</li>
-      <li>questionnaire en ligne</li>
-      <li>outils disponibles sur l’ENT : Madmagz, mur collaboratif, blog, cahier multimédia, pad</li>
-    </ul>
-    <p>Les productions seront à déposer pour le 17 janvier 2020 dans le groupe Numériqu’elles
-    créé sur l’ENT.</p>
-    <p>Un jury se réunira courant février : les réalisations les plus réussies seront récompensées et serviront à la promotion du forum l’année prochaine.</p>
-    <p>Les établissements qui le souhaitent peuvent être accompagnés dans ce projet par la Délégation académique au numérique éducatif : ce.dane@ac-amiens.fr</p>
-    <p>Chaque production, réalisée en groupe qui pourra être mixte, devra être accompagnée des éléments suivants (qui seront sur l’ENT) :</p>
-    <ul>
-      <li>la fiche de synthèse</li>
-      <li>la liste des élèves</li>
-      <li>les formulaires dûment remplis et signés de cession des droits d’auteur pour tous les élèves et les adultes participant au projet</li>
-      <li>le formulaire d’autorisation de captation de la voix et de l’image de l’élève dans le cas d’une vidéo</li>
-    </ul>
+  <div class="container_ambassadress">
+    <div>
+      <p>Devenez ambassadrices du numérique ! Vous venez de participer au forum Numériqu’elles 2020, il est proposé aux élèves des établissements ayant participé de réaliser une ou plusieurs productions utilisant le numérique pour promouvoir ce secteur auprès des collégiennes et des lycéennes notamment.</p>
+    </div>
+    <div>
+      <p>L’objectif est de déconstruire les stéréotypes liés au secteur du numérique en présentant par exemple un métier, une association, un parcours personnel et/ou professionnel, une action menée dans l’établissement en lien avec le forum...</p>
+    </div>
+    <div>
+      <p>Les réalisations possibles sont les suivantes :</p>
+    </div>
+    <img src="les_photos_drive/equality_girl_boy.png" class="logo_equality_girl_boy" alt="logo égalité filles garçons">
+    <div>  
+      <ul>
+        <li>- capsule vidéo de 3 minutes maximum</li>
+        <li>- émission d’une webradio</li>
+        <li>- page web du site de l’établissement</li>
+        <li>- diaporama</li>
+        <li>- questionnaire en ligne</li>
+        <li>- outils disponibles sur l’ENT : Madmagz, mur collaboratif, blog, cahier multimédia, pad</li>
+      </ul>
+    </div>
+    <div class="send_you_production">
+      <button>Les productions seront à envoyer à l’équipe à l'adresse :</button>
+    </div>
+    <div>
+      <p>Un jury se réunira courant mars : les réalisations les plus réussies seront récompensées et serviront à la promotion du forum l’année prochaine.</p>
+    </div>
+    <div>
+      <p class="important_information">Les établissements qui le souhaitent peuvent être accompagnés dans ce projet par la Délégation Académique au Numérique Educatif :  <a href="mailto:ce.dane@ac-lille.fr">ce.dane@ac-lille.fr</a></p>
+    </div>
+    <div>
+      <p>Chaque production, réalisée en groupe qui pourra être mixte, devra être accompagnée des éléments suivants (qui seront sur l’ENT) :</p>
+    </div>
+    <div>
+       <ul>
+        <li>- la fiche de synthèse</li>
+        <li>- la liste des élèves</li>
+        <li>- les formulaires dûment remplis et signés de cession des droits d’auteur pour tous les élèves et les adultes participant au projet</li>
+        <li>- le formulaire d’autorisation de captation de la voix et de l’image de l’élève dans le cas d’une vidéo</li>
+      </ul>
+    </div>
   </div>
   <?php
   require_once('footer.html');
--- prepare_yourself.php
+++ prepare_yourself.php
@@ -14,20 +14,21 @@
   <div>
     <h1>Se préparer à Numériqu’<span>Elles</span></h1>
   </div>
-  <section class="container_prepare_yourself">
-    <div>
+  <section class="container">
+  <div class="container_prepare_yourself">  
+    <div class="container_logo_onisep"></div>
+    <div class="container_text_prepare_yourself">
       <p>Vous êtes lycéen(ne) en CAP, en Bac Pro ou étudiant(e) en BTS et vous devez effectuer un stage ou une période de formation en entreprise : l'espace "Lycéens" de Mon stage en ligne vous est dédié.</p>
-    </div>
-    <div>
       <p>Vous y trouverez :</p>
       <ul>
-        <li>des offres de stage déposées par les entreprises et mettre celles qui vous intéressent dans votre classeur. Vous devrez transmette l'offre pour laquelle vous souhaitez postuler directement à votre professeur. Si cette offre est conforme à ce qui est attendu dans le cadre de votre formation,  il vous donnera son accord et les coordonnées de l'employeur.</li>
-        <li>la possibilité de déposer une demande de stage en ligne. Après validation par votre professeur, elle s'affichera sur l'espace "Entreprises employeurs" de Mon stage en ligne. Si un employeur se montre intéressé, il contactera le professeur référent pour les stages de votre classe.</li>
-        <li>une aide pour la rédaction de votre CV et de la lettre de motivation</li>
-        <li>une rubrique « Quels métiers avec mon diplôme ?"pour vous aider à construire votre CV, mettre en avant vos acquis, vos compétences, et  vous donner des idées de métiers !</li>
-        <li>des textes sur la convention de stage, vos droits et vos obligations, etc…</li>
+        <li>- des offres de stage déposées par les entreprises et mettre celles qui vous intéressent dans votre classeur. Vous devrez transmette l'offre pour laquelle vous souhaitez postuler directement à votre professeur. Si cette offre est conforme à ce qui est attendu dans le cadre de votre formation,  il vous donnera son accord et les coordonnées de l'employeur.</li>
+        <li>- la possibilité de déposer une demande de stage en ligne. Après validation par votre professeur, elle s'affichera sur l'espace "Entreprises employeurs" de Mon stage en ligne. Si un employeur se montre intéressé, il contactera le professeur référent pour les stages de votre classe.</li>
+        <li>- une aide pour la rédaction de votre CV et de la lettre de motivation</li>
+        <li>- une rubrique « Quels métiers avec mon diplôme ?"pour vous aider à construire votre CV, mettre en avant vos acquis, vos compétences, et  vous donner des idées de métiers !</li>
+        <li>- des textes sur la convention de stage, vos droits et vos obligations, etc…</li>
       </ul>
     </div>
+  </div>
   </section>
   <?php
   require_once('footer.html');
