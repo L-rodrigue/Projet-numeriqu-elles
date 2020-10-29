953dca31abcb21d266b561bc89aa7a77d1bd8701
Lepez_Rodrigue <lepezrodrigue1@gmail.com>
2020-10-29 10:32:46 +0100

    fusion resolu

--- numeriqu-elles-lille.php
+++ numeriqu-elles-lille.php
@@ -1,197 +1,52 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="numeriqu-elles.css">
-    <title>Numeriqu'elles Lille</title>
+    <title>Document</title>
 </head>
-
 <body>
-
-
-    <?php
-    require_once('header.html');
-?>
-    <h1>Numeriqu'<span>Elles </span>Lille</h1>
-    <div class='section'>
-        <p class='text'>Inscription via ce lien</p>
-        <input type="button" value="Cliquez ici">
-    </div>
-    <div class='section'>
-        <p class='text'>Salon virtuel</p>
-        <input type="button" value="Cliquez ici">
-    </div>
-    <p class='lock'>Collège rendez-vous à 9h00</p>
-    <p class='lock'>Lycée rendez-vous à 14h00</p>
-    <h2>Les ateliers partenaires</h2>
-    <div class="container">
-        <div>
-            <a href="https://upcyclecommons.wpcomstaging.com/"><img src="les_photos_drive/logo_upcycle.png" alt="logo"></a> 
-            <p> l’atelier <br>
-                Initiation au code et à l'art numérique</p>
-        </div>
-        <div>
-            <a href="https://www.euratechnologies.com/"><img src="les_photos_drive/Euratechnologies.jpeg" alt="logo"></a>
-            <p> l’atelier <br>
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-        <div>
-            <a href="https://www.ibm.com/fr-fr"><img src="les_photos_drive/logo_ibm.png" alt="logo"></a>
-            <p> l’atelier <br>
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-        <div>
-            <a href="https://www.axa.fr/"><img src="les_photos_drive/AXA_Logo.svg" alt="logo"></a>
-            <p> l’atelier <br>
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-        <div>
-						<a href="https://www.axa.fr/"><img src="les_photos_drive/AXA_Logo.svg" alt="logo"></a>
-            <p> l’atelier <br>
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-        <div>
-            <a href="https://www.axa.fr/"><img src="les_photos_drive/AXA_Logo.svg" alt="logo"></a>
-            <p> l’atelier <br>
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-        <div>
-            <a href="https://www.axa.fr/"><img src="les_photos_drive/AXA_Logo.svg" alt="logo"></a>
-            <p> l’atelier <br>
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-        <div>
-            <a href="https://www.axa.fr/"><img src="les_photos_drive/AXA_Logo.svg" alt="logo"></a>
-            <p> l’atelier <br>
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-        <div>
-            <a href="https://www.axa.fr/"><img src="les_photos_drive/AXA_Logo.svg" alt="logo"></a>
-            <p> l’atelier <br>
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-        <div>
-            <a href="https://www.decathlon.fr/"><img src="les_photos_drive/logo-Decathlon.jpg" alt="logo"></a>
-            <p> l’atelier <br>
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-        <div>
-            <a href="https://www.decathlon.fr/"><img src="les_photos_drive/logo-Decathlon.jpg" alt="logo"></a>
-            <p> l’atelier <br>
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-        <div>
-            <a href="https://www.decathlon.fr/"><img src="les_photos_drive/logo-Decathlon.jpg" alt="logo"></a>
-            <p> l’atelier <br>
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-        <div>
-            <a href="https://www.orientoi.fr/"><img src="les_photos_drive/orientoi.png" alt="logo"></a>
-            <p> l’atelier <br>
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-        <div>
-            <a href="https://simplon.co/"><img src="les_photos_drive/logo_simplon.png" alt="logo"></a>
-            <p> l’atelier <br>
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-        <div>
-            <a href="https://simplon.co/"><img src="les_photos_drive/logo_simplon.png" alt="logo"></a>
-            <p> l’atelier <br>
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-        <div>
-            <a href="https://www.laredoute.fr/"><img src="les_photos_drive/logo_laredoute.png" alt="logo"></a>
-            <p> l’atelier <br>
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-        <div>
-            <a href="https://www.laredoute.fr/"><img src="les_photos_drive/logo_laredoute.png" alt="logo"></a>
-            <p> l’atelier <br>
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-        <div>
-            <a href="https://www.laredoute.fr/"><img src="les_photos_drive/logo_laredoute.png" alt="logo"></a>
-            <p> l’atelier <br>
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-        <div>
-            <a href="https://www.laredoute.fr/"><img src="les_photos_drive/logo_laredoute.png" alt="logo"></a>
-            <p> l’atelier <br>
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-        <div>
-            <a href="http://www.face-mel.fr/?page_id=19"><img src="les_photos_drive/logo_facemel.png" alt="logo"></a>
-            <p> l’atelier <br>
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-        <div>
-            <a href="https://popschool.fr/"><img src="les_photos_drive/logo_pop.png" alt="pop-school"></a>
-            <p> l’atelier <br>
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-        <div>
-            <a href="https://www.corif.fr/"><img src="les_photos_drive/corif.png" alt="logo"></a>
-            <p> l’atelier <br>
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-        <div>
-            <a href="https://www.norsys.fr/"><img src="les_photos_drive/logo_fondation_norsys.png" alt="logo"></a>
-            <p> l’atelier <br>
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-        <div>
-            <a href="https://www.epitech.eu/"><img src="les_photos_drive/logo_epitech.png" alt="logo"></a>
-            <p> l’atelier <br>
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-        <div>
-            <a href="https://www.femmes-ingenieurs.org/"><img src="les_photos_drive/logo_fi.jpg" alt="logo"></a>
-            <p> l’atelier <br>
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-        <div>
-            <a href="https://www.hautsdefrance.fr/"><img src="les_photos_drive/Logo_Hauts-de-France_2016.svg" alt="logo"></a>
-            <p> l’atelier <br> 
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-        <div>
-            <a href="https://www.rte-france.com/"><img src="les_photos_drive/rte_nord.jpeg" alt="logo"></a>
-            <p> l’atelier <br>
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-        <div>
-            <a href="https://inria.fr/fr"><img src="les_photos_drive/logo_inria.png" alt="logo"></a>
-            <p> l’atelier <br>
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-        <div>
-            <a href="https://www.kiloutou.fr/"><img src="les_photos_drive/logo_kiloutou.png" alt="logo"></a>
-            <p> l’atelier <br>
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-        <div>
-            <a href="https://www.orange.fr/portail">x<img src="les_photos_drive/logo_orange.png" alt="logo"></a>
-            <p> l’atelier <br>
-                3 témoignages métiers de femmes dans le numérique</p>
-        </div>
-    </div>
-    <h2>Questionnaire de satisfaction</h2>
-    <div class='satifaction'>
-        <div class='text-satifaction'>
-            <p>Pour nous aider a améliorer nos atelier, veuillez répondre au questionnaire de satifaction qui est
-                anonyme, en le
-                téléchargeant ci-dessous et en le renvoyant à Maleka a l'adressse mail suivant:</p>
-        </div>
-        <div>
-            <input type="button" value="Télécharger le questionnaire">
-        </div>
-    </div>
-    <?php
-    require_once('footer.html');
-?>
+    <p>Numeriqu'<span>Elles </span>Lille</p>
+    <p>Inscription via ce lien</p>
+    <input type="button" value="Cliquez ici">
+    <p>Salon virtuel</p>
+    <input type="button" value="Cliquez ici">
+    <p>Collège rendez-vous à 9h00</p>
+    <p>Lycée rendez-vous à 14h00</p>
+    <p>Les ateliers partenaires</p>
+    <img src="les_photos_drive/logo_upcycle.png" alt="logo">
+    <img src="les_photos_drive/euratechnologies.jpeg" alt="logo">
+    <img src="les_photos_drive/logo_ibm.png" alt="logo">
+    <img src="les_photos_drive/AXA_Logo.svg" alt="logo">
+    <img src="les_photos_drive/AXA_Logo.svg" alt="logo">
+    <img src="les_photos_drive/AXA_Logo.svg" alt="logo">
+    <img src="les_photos_drive/AXA_Logo.svg" alt="logo">
+    <img src="les_photos_drive/AXA_Logo.svg" alt="logo">
+    <img src="les_photos_drive/AXA_Logo.svg" alt="logo">
+    <img src="les_photos_drive/logo_Decathlon.jpeg" alt="logo">
+    <img src="les_photos_drive/logo_Decathlon.jpeg" alt="logo">
+    <img src="les_photos_drive/logo_Decathlon.jpeg" alt="logo">
+    <img src="les_photos_drive/orientoi.png" alt="logo">
+    <img src="les_photos_drive/logo_simplon.png" alt="logo">
+    <img src="les_photos_drive/logo_simplon.png" alt="logo">
+    <img src="les_photos_drive/logo_laredoute.png" alt="logo">
+    <img src="les_photos_drive/logo_laredoute.png" alt="logo">
+    <img src="les_photos_drive/logo_laredoute.png" alt="logo">
+    <img src="les_photos_drive/logo_laredoute.png" alt="logo">
+    <img src="les_photos_drive/logo_facemel.png" alt="logo">
+    <img src="les_photos_drive/" alt="pop-school">
+    <img src="les_photos_drive/corif.png" alt="logo">
+    <img src="les_photos_drive/logo_fondation_norsys.png" alt="logo">
+    <img src="les_photos_drive/logo_epitech.png" alt="logo">
+    <img src="les_photos_drive/logo_fi.png" alt="logo">
+    <img src="les_photos_drive/Logo_Hauts-de-France_2016.svg" alt="logo">
+    <img src="les_photos_drive/rte_nord.jpeg" alt="logo">
+    <img src="les_photos_drive/logo_inria.png" alt="logo">
+    <img src="les_photos_drive/logo_kiloutou.png" alt="logo">
+    <img src="les_photos_drive/logo_orange.png" alt="logo">
+    <p>Questionnaire de satisfaction</p>
+    <p>Pour nous aider a améliorer nos atelier, veuillez répondre au questionnaire de satifaction qui est anonyme, en le téléchargeant ci-dessous et en le renvoyant à Maleka a l'adressse mail suivant:</p>
+    <input type="button" value="Télécharger le questionnaire">
 </body>
-
 </html>
+
