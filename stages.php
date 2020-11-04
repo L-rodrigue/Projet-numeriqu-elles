4593ca4375e07aa46a77a1487686c29e4330952e
Lepez_Rodrigue <lepezrodrigue1@gmail.com>
2020-11-04 06:08:17 +0100

    add contenu

--- stages.php
+++ stages.php
@@ -3,9 +3,62 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="stages.css">
+  <title>Stages</title>
 </head>
 <body>
+  <?php
+  require_once('header.html')
+  ?>
+  <h1>Les <span>stages</span></h1>
+  <div class="text">
+  <p>Si vous souhaitez prolonger votre expérience du numérique, vous pouvez trouver un stage proposé par
+     les entreprises partenaires (en fonction de l’évolution de la crise sanitaire)
+     en utilisant les plateformes de stage dédiées </p>
+    </div>
+  <div class="container">
+    <div class="stage">
+      <div class="picture">
+      <img src="les_photos_drive/AnoukDesury-Numeriquelles2019-17.jpg" alt="photo">
+    </div>
+      <a href="https://nordcolleges.enthdf.fr/mon-stage-de-3eme/">
+      <p>Mon Stage 3ème département du nord </p>
+    </a>
+    </div>
+    <div class="stage">
+      <div class="picture">
+      <img src="les_photos_drive/AnoukDesury-Numeriquelles2019-27.jpg" alt="photo">
+    </div>
+      <a href="https://www.monstageenligne.fr/">
+      <p></p>
+    </a>
+    </div>
+    <div class="stage">
+      <div class="picture">
+      <img src="les_photos_drive/AnoukDesury-Numeriquelles2019-54.jpg" alt="photo">
+    </div>
+      <a href="https://www.monstagedetroisieme.fr/">
+      <p>Des solutions pour vous accompagner avant, pendant, et après votre stage</p>
+    </a>
+    </div>
+    <div class="stage">
+      <div class="picture">
+      <img src="les_photos_drive/AnoukDesury-Numeriquelles2019-133.jpg" alt="photo">
+    </div>
+      <a href="https://stage3e.pasdecalais.fr/">
+      <p>Zéro collégien sans stage</p>
+    </a>
+    </div>
+  </div>
+  <div class="text">
+  <p>Vous souhaitez être accompagné dans votre recherche de stage ?
+     Contactez Murielle Delrot, Ingénieure pour l’école à la Délégation de Région Académique à la Formation Professionnelle
+      Initiale et Continue </p>
+    </div>
+  <?php
+  require_once('footer.html')
+  ?>
   
 </body>
 </html>
