34f3f328fd73bdace2673453f06d84e2274bd3ea
Lepez_Rodrigue <lepezrodrigue1@gmail.com>
2020-11-04 07:14:58 +0100

    correction elements

--- numeriqu-elles-lens.php
+++ numeriqu-elles-lens.php
@@ -20,97 +20,81 @@
         <p class='text'>Inscription via ce lien</p>
         <input type="button" value="Cliquez ici">
     </div>
-    <div class='section'>
-        <p class='text'>Salon virtuel</p>
-        <input type="button" value="Cliquez ici">
-    </div>
-    <p class='lock'>Collège rendez-vous à 9h00</p>
-    <p class='lock'>Lycée rendez-vous à 14h00</p>
     <h2>Les ateliers partenaires</h2>
     <div class="containers">
     <div class='container'>
             <a href="https://www.orientoi.fr/"><img src="les_photos_drive/orientoi.png" alt="logo"></a>
-            <p> l’atelier <br>
-                Jeux des talents <br>
-                découverte des métiers du <br>
-                numérique présentation <br>
-                du fonctionnement de <br>
-                l'intelligence artificille</p>
         </div>
         <div class='container'>
             <a href="https://www.norsys.fr/"><img src="les_photos_drive/logo_fondation_norsys.png" alt="logo"></a>
-            <p> l’atelier <br>
-                Casser les stéréotypes <br>
-                des métiers de l'informatique</p>
         </div>
         <div class='container'>
             <a href="https://www.epitech.eu/"><img src="les_photos_drive/logo_epitech.png" alt="logo"></a>
-            <p> l’atelier <br>
-                Pauline témoigne: <br>
-                pourquoi/comment le choix <br> 
-                du numérique/programation <br> 
-                informatique <br>  
-                illustrations concrètes</p>
         </div>
     </div>
     <div>
-    <h2>le programme de votre établissement</h2>
-    <div class='section2'>
-   <p> Merci de veiller à ce que toutes les contraintes techniques soient prises en compte avant le jour J afin que nous puissions tous profiter de ce forum dans les meilleures conditions.
-Nous comptons sur les encadrants pour jouer le rôle de modérateur afin d’animer et réguler les échanges en collaboration avec nos partenaires sur les ateliers.
-Les jeunes filles pourront devenir « Ambassadrices du numérique » (lien vers page ambassadrices) en participant à ce nouveau concours.
-Enfin, si certaines d’entre elles souhaitent prolonger leur expérience du numérique, elles pourront trouver un stage proposé par nos entreprises partenaires (en fonction de l’évolution de la crise sanitaire) (lien vers page stage).</p>
-
-<p>Les collèges
-8H30-9H00 : Début des connexions (tests de connexion, audio et vidéo)
-9H00-9H30 : Séance plénière afin d’introduire et de présenter l’organisation du forum
-9H30-12H00 : 4 à 5 ateliers pour découvrir le numérique en respectant votre planning
-12H00-12H15 : Mot de la fin
-Planning Collège 1
-Planning Collège 2
-Planning Collège 3
-</p> 
-<p>
-Les Lycées
-13H30-14H00 : Début des connexions (tests de connexion, audio et vidéo)
-14H00-14H30 : Séance plénière afin d’introduire et de présenter l’organisation du forum
-14H30-17H00 : 4 à 5 ateliers pour découvrir le numérique en respectant votre planning
-17H00-17H15 : Mot de la fin
-Planning Lycée 1
-Planning Lycée2
-...
-</p>
-
-    </div>
-    <div class='section'>
-        <p class='text'>Salon virtuel</p>
-        <input type="button" value="Cliquez ici">
-    </div>
     <div>
-    <p>Si vous rencontrez des difficultés à vous connecter au salon virtuel, consultezle PDF
-Par mesure de sécurité, si la visioconférence VIA n’est pas fonctionnelle,
-les partenaires ont prévu un lien Teams de secours pour proposer leur atelier. 
-Merci de vérifier que l’accès à Teams est possible sur le réseau de votre établissement.
-</p>
- </div>
-</div>
-<div>
-    <p>
-        N’oubliez pas de faire remplir le questionnaire de satisfaction par vos élèves. 
-</p>
-<p>
-l’envoyer à l’adresse mail de maleka :mdilmi@corif.fr
-</p>
-</div>
+        <h2>le programme de votre établissement</h2>
+        <div class='section2'>
+            <p> Merci de veiller à ce que toutes les contraintes techniques soient prises en compte avant le jour J afin <br>
+                que nous puissions tous profiter de ce forum dans les meilleures conditions. <br>
+                Nous comptons sur les encadrants pour jouer le rôle de modérateur afin d’animer et réguler les échanges <br>
+                en collaboration avec nos partenaires sur les ateliers. <br>
+                Les jeunes filles pourront devenir « Ambassadrices du numérique » (lien vers page ambassadrices) en <br>
+                participant à ce nouveau concours. <br>
+                Enfin, si certaines d’entre elles souhaitent prolonger leur expérience du numérique, elles pourront <br>
+                trouver un stage proposé par nos entreprises partenaires (en fonction de l’évolution de la crise <br>
+                sanitaire) (lien vers page stage).</p>
+
+            <p><span>Les collèges</span> <br>
+                8H30-9H00 : Début des connexions (tests de connexion, audio et vidéo) <br>
+                9H00-9H30 : Séance plénière afin d’introduire et de présenter l’organisation du forum <br>
+                9H30-12H00 : 4 à 5 ateliers pour découvrir le numérique en respectant votre planning <br>
+                12H00-12H15 : Mot de la fin <br>
+                Planning Collège 1 <br>
+                Planning Collège 2 <br>
+                Planning Collège 3 <br>
+            </p>
+            <p><span>Les Lycées</span> <br>
+                13H30-14H00 : Début des connexions (tests de connexion, audio et vidéo) <br>
+                14H00-14H30 : Séance plénière afin d’introduire et de présenter l’organisation du forum <br>
+                14H30-17H00 : 4 à 5 ateliers pour découvrir le numérique en respectant votre planning <br>
+                17H00-17H15 : Mot de la fin <br>
+                Planning Lycée 1 <br>
+                Planning Lycée2 <br>
+                Planning Lycée3
+            </p>
+
+        </div>
+        <div class='section2'>
+            <div class="section">
+            <p class='text'>Salon virtuel</p>
+            <input type="button" value="Cliquez ici">
+        </div>
+        
+            <p>Si vous rencontrez des difficultés à vous connecter au salon virtuel, consultezle PDF
+                Par mesure de sécurité, si la visioconférence VIA n’est pas fonctionnelle,
+                les partenaires ont prévu un lien Teams de secours pour proposer leur atelier.
+                Merci de vérifier que l’accès à Teams est possible sur le réseau de votre établissement.
+            </p>
+        <p>
+            N’oubliez pas de faire remplir le questionnaire de satisfaction par vos élèves.
+        </p>
+        <p>
+            l’envoyer à l’adresse mail de maleka :mdilmi@corif.fr
+        </p>
+    </div>
+    </div>
     <h2>Questionnaire de satisfaction</h2>
     <div class='satifaction'>
         <div class='text-satifaction'>
             <p>Pour nous aider a améliorer nos atelier, veuillez répondre au questionnaire de satifaction qui est
                 anonyme, en le
                 téléchargeant ci-dessous et en le renvoyant à Maleka a l'adressse mail suivant:</p>
-                <a href="https://drive.google.com/file/d/1PFpiost2ZQtKJQVrMep0qgHCOM3Ju9aC/view?usp=sharing "><input type="button" value="Télécharger le questionnaire"></a>
+            <a href="https://drive.google.com/file/d/1PFpiost2ZQtKJQVrMep0qgHCOM3Ju9aC/view?usp=sharing "><input
+                    type="button" value="Télécharger le questionnaire"></a>
         </div>
-        <div class= "pdf">
+        <div class="pdf">
         </div>
     </div>
     <?php
